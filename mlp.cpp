917bd33d912a3e174c23306c09cbafc860a224c2
prithvichakra@gmail.com <prithvichakra@gmail.com>
2016-03-21 02:29:49 -0400

    MLP up and running. See tester.cpp for usage

---
+++ mlp.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include "net.h"
+using namespace std;
+
+
+Net::Net(){
+    wt = new float**[MAX_LAYERS];
+    del_w = new float**[MAX_LAYERS];
+    lay_count = 0;
+    init_type = RAND;
+    node_count = new int[MAX_LAYERS];
+    lay_type = new int[MAX_LAYERS];
+    fan_in = new float*[MAX_LAYERS];
+    fan_f = new float*[MAX_LAYERS];
+    del_in = new float*[MAX_LAYERS];
+    del_f = new float*[MAX_LAYERS];
+    in_len = 0; out_len = 0;
+    lrate = 0.01;
+    mtm = 0.0;
+    total_error = 99999;
+    prev_error = 0;
+    srand(time(NULL));
+    updated = true;
+}
+
+void Net::add_layer(int len, int out_mode, int type){
+    if(out_mode == HIDDEN){
+        len++;
+        out_len = len-1;
+    }
+    else
+        out_len = len;
+    node_count[lay_count] = len;
+    lay_type[lay_count] = type;
+    if(lay_count == 0){
+        lay_count++;
+        if(out_mode == HIDDEN)
+            in_len = len-1;
+        else
+            in_len = len;
+        return;
+    }
+    int prev_len = node_count[lay_count-1];
+    wt[lay_count-1] = new float*[len];
+    del_w[lay_count-1] = new float*[len];
+    for(int i = 0; i < len; i++){
+        wt[lay_count-1][i] = new float[prev_len];
+        del_w[lay_count-1][i] = new float[prev_len];
+        for(int j = 0; j < prev_len; j++){
+            switch(init_type){
+                case RAND: wt[lay_count-1][i][j] = (rand()%10000)/10000.0; //Random initialzation
+                           break;
+                case ONE:  wt[lay_count-1][i][j] = 1;
+                           break;
+            }
+            del_w[lay_count-1][i][j] = 0;
+        }
+    }
+    lay_count++;
+}
+
+float * Net::mul(float ** w, int r, int c, float * vec){
+    float * res;
+    res = new float[r];
+    for(int i = 0; i < r; i++){
+        res[i] = 0;
+        for(int j = 0; j < c; j++){
+            res[i] += w[i][j]*vec[j];
+        }
+    }
+    return res;
+}
+
+float * Net::feed(float * input){
+    for(int i = 0; i < lay_count-1; i++){
+        if(i == 0){
+            fan_in[i] = new float[node_count[i]-1];
+            fan_f[i] = new float[node_count[i]];
+            for(int j = 0; j < in_len; j++){
+                fan_in[i][j] = input[j];
+                fan_f[i][j] = input[j];
+            }
+            fan_f[i][node_count[i]-1] = 1;
+        }
+        fan_in[i+1] = mul(wt[i],node_count[i+1],node_count[i]-1,fan_f[i]);
+        fan_f[i+1] = new float[node_count[i+1]];
+        int l = node_count[i+1]-1;
+        if(i+1 == lay_count-1)
+            l = node_count[i+1];
+        for(int j = 0; j < l; j++){
+            fan_f[i+1][j] = activation(fan_in[i+1][j],lay_type[i+1]);
+        }
+        if(i+1 < lay_count-1)
+            fan_f[i+1][node_count[i+1]-1] = 1;
+    }
+    
+    return fan_f[lay_count-1];
+}
+
+//Add one input for training
+void Net::add_train(float * input, float * target){
+    float * res = feed(input);
+    for(int i = lay_count-1; i >= 0; i--){
+        del_in[i] = new float[node_count[i]];
+        if(i == lay_count-1){
+            for(int j = 0; j < out_len; j++){
+                float diff = (target[j]-res[j]);
+                del_in[i][j] = diff*activation(fan_in[i][j],lay_type[i]+DER);
+                total_error += diff*diff;
+            }
+            continue;
+        }
+        //Calculate del_j
+        for(int j = 0; j < node_count[i]; j++){
+            del_in[i][j] = 0;
+            for(int k = 0; k < node_count[i+1]; k++)
+                del_in[i][j] += del_in[i+1][k]*wt[i][k][j];
+            del_in[i][j] *= activation(fan_in[i][j],lay_type[i]+DER);
+        }
+        for(int j = 0; j < node_count[i]; j++){
+            for(int k = 0; k < node_count[i+1]; k++){
+                del_w[i][k][j] *= mtm;
+                del_w[i][k][j] += lrate*del_in[i+1][k]*fan_f[i][j];
+            }
+        }
+    }
+    updated = false;
+}
+
+//Update weights and adaptive learning rates
+void Net::update_wt(bool adaptive_lrate){
+    bool bad = false;
+    if(adaptive_lrate){
+        bad = ((total_error - prev_error) > L_THRESH);
+        if(bad)
+            if(lrate > MIN_LRATE)
+                lrate *= MOD_LRATE_DEC;
+        else
+            if(lrate < MAX_LRATE)
+                lrate *= MOD_LRATE_INC;
+    }
+    for(int i = 0; i < lay_count-1; i++)
+        for(int j = 0; j < node_count[i+1]; j++)
+            for(int k = 0; k < node_count[i]; k++){
+                if(!bad)
+                    wt[i][j][k] += del_w[i][j][k];// + mtm*wt[i][j][k];
+                del_w[i][j][k] = 0;
+            }
+    updated = true;
+}
--- tester.cpp
+++ tester.cpp
@@ -4,32 +4,29 @@ using namespace std;
 
 int main(){
     Net net;
-    net.add_layer(1,HIDDEN,SIGM);
-    //net.add_layer(10,HIDDEN,SIGM);
-    net.add_layer(10,HIDDEN,SIGM);
-    //net.add_layer(10,HIDDEN,SIGM);
-    net.add_layer(1,OUTPUT,SIGM);
+    net.add_layer(1,HIDDEN,TANH);
+    net.add_layer(10,HIDDEN,TANH);
+    net.add_layer(10,HIDDEN,TANH);
+    net.add_layer(10,HIDDEN,TANH);
+    net.add_layer(1,OUTPUT,TANH);
     
     net.train("./data", //Path to training data
-              "./model_cos_sq_one_lobe", //Path to destination folder
+              "./model_cos_tanh", //Path to destination folder
               1, //Mini batch size (=1 for online learning)
-              0.02, //Learning rate
+              0.001, //Learning rate
+              false, //Turn on adaptive learning rates
+              0.0, //Momentum
               1, //Saves intermediate models every CYC batches later
               true, //Switch to turn verbose feature on/off
-              20000 //Maximum number of epochs
+              1000 //Maximum number of epochs
              );
     
-    net.load("./model_cos_sq_one_lobe");
+    net.load("./model_cos_tanh");
 
-    for(int i = 0; i < 314; i++){
+    for(int i = 0; i < 628; i++){
         float inp = i/100.0;
         cout<<*net.feed(&inp)<<", ";
     }
     cout<<endl;
-    /*for(int i = 0; i < 314; i+=(314/8)){
-        float inp = i/100.0;
-        cout<<inp<<", ";
-    }*/
-    cout<<endl;
-    //net.print();
+    net.print();
 }
